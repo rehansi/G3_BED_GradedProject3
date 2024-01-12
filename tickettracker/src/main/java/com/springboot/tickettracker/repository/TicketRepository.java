72d962b86c656d0aa6afffda72fd42120c4501c5
rehansi <150148649+rehansi@users.noreply.github.com>
2024-01-12 15:18:37 +0530

    Add files via upload

---
+++ tickettracker/src/main/java/com/springboot/tickettracker/repository/TicketRepository.java
@@ -0,0 +1,12 @@
+package com.springboot.tickettracker.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.tickettracker.model.Ticket;
+
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    List<Ticket> findByTitleContainingOrShortDescriptionContaining(String title, String shortDescription);
+}
